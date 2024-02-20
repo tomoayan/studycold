6d02ec6f422a3a0739030c27f3e88cc681714f18
tomoayan <63101155+tomoayan@users.noreply.github.com>
2024-02-20 20:25:49 +0530

    added github workflow

--- site/.vitepress/config.mjs
+++ site/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "studycold",
   description: "A VitePress Site",
+  base: '/studycold/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
